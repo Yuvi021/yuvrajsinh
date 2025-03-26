3fb44324b7b2c0349263df925f8c6dd440d4970c
yash <yash.vibhandik@swyftbooking.com>
2025-03-26 17:12:12 +0530

    fix build

--- src/components/SuccessModal.tsx
+++ src/components/SuccessModal.tsx
@@ -1,7 +1,7 @@
 // src/components/SuccessModal.tsx
 import { motion } from "framer-motion";
 
-const SuccessModal = ({ onClose }) => {
+const SuccessModal = ({ onClose }:any) => {
   return (
     <motion.div
       className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
